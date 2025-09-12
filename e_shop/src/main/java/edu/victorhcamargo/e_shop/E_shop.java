cb8d1db7bea96b6626c50bda19955d007f171d21
VictorHCamargo <victor.h0786@gmail.com>
2025-09-11 22:00:20 -0300

    Aula-3 11/09

--- e_shop/src/main/java/edu/victorhcamargo/e_shop/E_shop.java
+++ e_shop/src/main/java/edu/victorhcamargo/e_shop/E_shop.java
@@ -5,6 +5,9 @@
 package edu.victorhcamargo.e_shop;
 
 import Produto.Produto;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -12,21 +15,53 @@ import Produto.Produto;
  */
 public class E_shop {
 
-    public static void main(String[] args) {
-        Produto topeira = new Produto();
-        topeira.setNome("Teste 1");
-        topeira.setCodigo(1);
-        topeira.setPreco((float) 24.90);
-        topeira.setSaldo((float)34.90);
-        topeira.setDescricao("Esse e meu primeiro teste");
+    public static void main(String[] args) throws IOException{
+        Produto topeira1 = new Produto(0);
+        topeira1.setNome("Teste 1");
+        topeira1.setPreco((float) 24.90);
+        topeira1.setSaldo((float)34.90);
+        topeira1.setDescricao("Esse e meu primeiro teste");
+        
+        Produto topeira2 = new Produto(1);
+        topeira2.setNome("Teste 2");
+        topeira2.setPreco((float) 104.90);
+        topeira2.setSaldo((float)3.90);
+        topeira2.setDescricao("Esse e meu segundo teste");
+        
+        Produto topeira3 = new Produto(2);
+        topeira3.setNome("Teste 3");
+        topeira3.setPreco((float) 90.78);
+        topeira3.setSaldo((float) 4.90);
+        topeira3.setDescricao("Esse e meu terceiro teste");
+        
+        Produto topeira4 = new Produto(3);
+        
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(isr);
+        int qtd;
+        qtd = Integer.parseInt(
+                                   reader.readLine()
+        );
+        String nome;
+        nome = "\nDigite seu nome: " + reader.readLine();
+        float preco;
+        preco = Float.parseFloat(reader.readLine());
+        float saldo;
+        saldo = Float.parseFloat(reader.readLine());
+        String descricao;
+        descricao = reader.readLine();
+        topeira4.setNome(nome);
+        topeira4.setPreco(preco);
+        topeira4.setSaldo(saldo);
+        topeira4.setDescricao(descricao);
         System.out.println(
-                "Venda:"
-                + " " + topeira.getNome()
-                + " " + topeira.getDescricao()
-                + " COD:" + topeira.getCodigo()
-                + " Valor: " + topeira.getPreco()
-                + " Saldo:" + topeira.getSaldo()
-                + " Ao comprar: " + (topeira.getSaldo() - topeira.getPreco())
+                "Digite seu nome: \n" + nome + "\nDigite o preço do produto: \n"
+                + preco + "\nDigite o saldo disponível:\n" + saldo
+                + "\nDigite a descricao do produto:\n " + descricao +
+                "Venda: "
+                + topeira4.getCodigo() + " "+ topeira4.getNome()
+                + topeira4.getPreco()+ "x"+ qtd + " ="
+                + (qtd*topeira4.getPreco())
         );
     }
 }
--- e_shop/src/main/java/produto/Produto.java
+++ e_shop/src/main/java/produto/Produto.java
@@ -10,18 +10,54 @@
  */
 package Produto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Produto {
     private String nome;
     private float saldo;
     private int codigo;
     private float preco;
     private String descricao;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Produto(int codigo) {
+        this.codigo = codigo;
+    }
 }
