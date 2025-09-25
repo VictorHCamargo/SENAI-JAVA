c8f8f2e11ba4177fb6d0b3dad05a194dc3409ad7
VictorHCamargo <victor.h0786@gmail.com>
2025-09-24 21:57:16 -0300

    Ajustes Aula-4

---
+++ padaria/src/main/java/edu/victorhcamargo/padaria/Padaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.victorhcamargo.padaria;
+
+/**
+ *
+ * @author Java_blessed
+ */
+public class Padaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ padaria/src/main/java/model/Produto.java
@@ -0,0 +1,46 @@
+package model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Java_blessed
+ */
+public class Produto {
+    private int Codigo;
+    private double Preco;
+    private String Descricao;
+    public Produto(int Codigo, double Preco, String Descricao) {
+        this.Codigo = Codigo;
+        this.Preco = Preco;
+        this.Descricao = Descricao;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public double getPreco() {
+        return Preco;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public void setPreco(double Preco) {
+        this.Preco = Preco;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+}
